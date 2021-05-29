d0eacd02bd4f1cd06fd4169b856845e36d640d2c
VoyagerOne <excelgeek@gmail.com>
2021-05-28 21:38:50 -0400

    Version 2  bench:  3918314

--- src/search.cpp
+++ src/search.cpp
@@ -1480,7 +1480,7 @@ moves_loop: // When in check, search starts from here
           &&  type_of(move) != PROMOTION)
       {
 
-          if (moveCount > 2)
+          if (moveCount > 2 - int(depth<-3))
               continue;
 
           futilityValue = futilityBase + PieceValue[EG][pos.piece_on(to_sq(move))];
