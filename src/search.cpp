d2d0db187fef1abf76a7bd03957512362b59c529
VoyagerOne <excelgeek@gmail.com>
2021-06-01 14:42:46 -0400

    More reduction if likely to fail low at ttCapture bench:  4110023

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop: // When in check, search starts from here
           {
               // Increase reduction if ttMove is a capture (~3 Elo)
               if (ttCapture)
-                  r++;
+                  r += 1 + likelyFailLow;
 
               ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                              + (*contHist[0])[movedPiece][to_sq(move)]
