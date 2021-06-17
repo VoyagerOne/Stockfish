ac0b83168de2575998e4a4803995e2725c5a8d12
VoyagerOne <excelgeek@gmail.com>
2021-06-17 11:06:34 -0400

    Not for excluded moves

--- src/search.cpp
+++ src/search.cpp
@@ -1680,6 +1680,7 @@ moves_loop: // When in check, search starts from here
     // Extra penalty for a quiet early move that was not a TT move or
     // main killer move in previous ply when it gets refuted.
     if (   ((ss-1)->moveCount == 1 + (ss-1)->ttHit || ((ss-1)->currentMove == (ss-1)->killers[0]))
+        && !(ss-1)->excludedMove
         && !pos.captured_piece())
             update_continuation_histories(ss-1, pos.piece_on(prevSq), prevSq, -bonus1);
 
