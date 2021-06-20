4cca92bba3c8ad0138359cc61a896a4300f05567
VoyagerOne <excelgeek@gmail.com>
2021-06-19 21:14:37 -0400

    Ver 2 bench:  4684845

--- src/search.cpp
+++ src/search.cpp
@@ -1143,7 +1143,7 @@ moves_loop: // When in check, search starts from here
           Depth r = reduction(improving, depth, moveCount);
 
           if (PvNode)
-              r--;
+              r-=1+!ttMove;
 
           // Decrease reduction if the ttHit running average is large (~0 Elo)
           if (thisThread->ttHitAverage > 537 * TtHitAverageResolution * TtHitAverageWindow / 1024)
