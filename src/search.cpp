13ff0663a96eeb159e537e3bea57c7312e141fe7
VoyagerOne <excelgeek@gmail.com>
2021-07-13 09:44:46 -0400

    See what happens when not saving Eval in TT.  Based off and against ttEM bench:  4821289

--- src/search.cpp
+++ src/search.cpp
@@ -758,10 +758,6 @@ namespace {
             ss->staticEval = eval = evaluate(pos);
         else
             ss->staticEval = eval = -(ss-1)->staticEval;
-
-        // Save static evaluation into transposition table
-        if(!excludedMove)
-        tte->save(posKey, VALUE_NONE, ss->ttPv, BOUND_NONE, DEPTH_NONE, MOVE_NONE, eval);
     }
 
     // Use static evaluation difference to improve quiet move ordering
