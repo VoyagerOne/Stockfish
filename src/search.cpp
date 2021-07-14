acc93c58b72611c654fc147c1d04e6e6d2552087
VoyagerOne <excelgeek@gmail.com>
2021-07-14 11:40:57 -0400

    Ver 2 bench:  5016856

--- src/search.cpp
+++ src/search.cpp
@@ -1099,6 +1099,12 @@ moves_loop: // When in check, search starts from here
                && abs(ss->staticEval) > Value(100))
           extension = 1;
 
+      else if (PvNode
+          && ttMove
+          && ss->inCheck
+          && MoveList<LEGAL>(pos).size() == 1)
+          extension = 1;
+
       // Add extension to new depth
       newDepth += extension;
       ss->doubleExtensions = (ss-1)->doubleExtensions + (extension == 2);
