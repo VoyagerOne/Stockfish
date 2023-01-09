99d806d1be52fc048907c162182945cee086d681
VoyagerOne <excelgeek@gmail.com>
2023-01-09 16:29:31 -0500

    stc bench:  3756444

--- src/search.cpp
+++ src/search.cpp
@@ -1217,7 +1217,11 @@ moves_loop: // When in check, search starts here
       {
                // Increase reduction for cut nodes and not ttMove (~1 Elo)
                if (!ttMove && cutNode)
-                         r += 2;
+                         r += 1;
+
+               if (cutNode)
+                   r++;
+
 
                value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, newDepth - (r > 4), !cutNode);
       }
