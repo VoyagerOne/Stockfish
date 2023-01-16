41372a070d43c04adb768a5f148ec39fddcf3193
VoyagerOne <excelgeek@gmail.com>
2023-01-16 13:23:38 -0500

    stc bench:  3840985

--- src/search.cpp
+++ src/search.cpp
@@ -793,7 +793,7 @@ namespace {
         && (ss-1)->currentMove != MOVE_NULL
         && (ss-1)->statScore < 18200
         &&  eval >= beta
-        && !(ss-1)->didExt
+       // && !(ss-1)->didExt
         &&  eval >= ss->staticEval
         &&  ss->staticEval >= beta - 20 * depth - improvement / 14 + 235 + complexity / 24
         && !excludedMove
@@ -846,7 +846,7 @@ namespace {
     // much above beta, we can (almost) safely prune the previous move.
     if (   !PvNode
         &&  depth > 4
-        //&& !(ss-1)->didExt
+        && !(ss-1)->didExt
         &&  abs(beta) < VALUE_TB_WIN_IN_MAX_PLY
         // if value from transposition table is lower than probCutBeta, don't attempt probCut
         // there and in further interactions with transposition table cutoff depth is set to depth - 3
