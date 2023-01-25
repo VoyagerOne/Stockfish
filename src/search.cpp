b58249dfd69ac7c772a2f1196d0dd47cf6d8b1a2
VoyagerOne <excelgeek@gmail.com>
2023-01-25 09:00:57 -0500

    stc bench:  3918400

--- src/search.cpp
+++ src/search.cpp
@@ -1177,6 +1177,11 @@ moves_loop: // When in check, search starts here
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
       r -= ss->statScore / (12800 + 4410 * (depth > 7 && depth < 19));
 
+
+      if (r > 4 && extension == 2)
+          r--;
+
+
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
       // been searched. In general we would like to reduce them, but there are many
